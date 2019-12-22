c0bcd48d40230fd7bf5479852d845d7a45722ce8
thegalkin <thegalkin@users.noreply.github.com>
2019-12-22 23:25:29 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,6 +1,6 @@
 import telebot
-bot = telebot.TeleBot('%773838642:AAFvElKnWJZMjyOiegGxLPDLneASy4Hpmaw%')
-@bot.message_handler(content_types=['text'])
+bot = telebot.TeleBot('773838642:AAFvElKnWJZMjyOiegGxLPDLneASy4Hpmaw')
+message = bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "Привет" or message.text == "Начали" or message.text == "Покажи кинотеатры" or message.text == "Здравствуйте" or message.text == "Привет бот":
         bot.send_message(message.from_user.id,"Привет, выбери сеть кинотеатров: 1)Каро 2)... 3)...")
@@ -8,3 +8,5 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Прости, я тебя не понимаю. Напиши мне Привет")
 
     bot.polling(none_stop=True, interval=0)
+while 1==1:
+    get_text_messages(message)
