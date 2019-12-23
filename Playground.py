b81487e0060cba47828cf3d52a9264dc163c5019
thegalkin <thegalkin@users.noreply.github.com>
2019-12-23 08:50:31 +0300

    Update Playground.py

--- Playground.py
+++ Playground.py
@@ -2,13 +2,15 @@ from bs4 import BeautifulSoup as BF
 import re
 import requests as req
 import sqlite3
-karo = {1: {"скайокер": ["12:30"]}, 2: {"бурунов дома" : ["13:50"]}}
-cinemas = {"cinme": {"metro" : "marino", "data-id" : "43"}}
+karo = {1: {"скайокер": ["12:30"]}, 2: {"один дома" : ["13:50"]}}
+cinemas = {"cinema's name": {"metro" : "marino", "data-id" : "43"}, "uc" :{"metro" : "bunino",  "data-id" : "54"}}
+checker = {1: "43", 2: "54"}
 #for i in karo:
  #   print(karo[i].keys())
+if "54" in checker.values():
+    print("yes")
 for i in cinemas.values():
-
-    print(i.values())
+    print(i.get("data-id"))
 
 
 
