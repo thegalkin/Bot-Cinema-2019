a01f331ec5984c276bbd73af3e87c1283d575e1a
thegalkin <thegalkin@users.noreply.github.com>
2019-12-23 07:10:30 +0300

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup as BF
 import requests as req
 
-urls = "https://karofilm.ru/theatres"
+
 todayKARO = open("Today_Parser_KARO_raw.txt", 'w')
 contentsKARO = req.get(urls)
 contentsKARO = BF(contentsKARO.text, "html.parser")
 todayKARO.write(str(contentsKARO))
+
